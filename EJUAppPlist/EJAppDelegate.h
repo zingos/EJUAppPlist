031230a55e7a29edb06fc3df98f0491b0795957c
zanling <zanling@zing.local>
2014-06-26 13:05:04 +0800

    Initial Commit

---
+++ EJUAppPlist/EJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EJAppDelegate.h
+//  EJUAppPlist
+//
+//  Created by zanling on 6/26/14.
+//  Copyright (c) 2014 zanling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EJUAppPlist/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EJUAppPlist
+//
+//  Created by zanling on 6/26/14.
+//  Copyright (c) 2014 zanling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EJAppDelegate class]));
+    }
+}
